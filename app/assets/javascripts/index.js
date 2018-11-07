c4e6fce407fb9ef0e4dcc37dc52e1610fa46dc2c
takaikunoff <garakuta.guitar.ist@gmail.com>
2018-11-07 11:31:04 +0900

    add incremental search function

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,38 @@
+$(function() {
+var search_list = $("#user-search-result");
+
+function appendUser(user) {
+   var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    search_list.append(html);
+ }
+
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーは見つかりませんでした");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+});
