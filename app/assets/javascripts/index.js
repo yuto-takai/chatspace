e725faa8332bf922d0e1c134a24afa458e822ef1
takaikunoff <garakuta.guitar.ist@gmail.com>
2018-11-07 18:20:46 +0900

    add-user/remove-user button to create group

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 $(function() {
 var search_list = $("#user-search-result");
+var group_users_list = $("#chat-group-users");
 
 function appendUser(user) {
    var html = `<div class="chat-group-user clearfix">
@@ -9,6 +10,14 @@ function appendUser(user) {
     search_list.append(html);
  }
 
+function appendGroupUser(user_id, user_name) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                <p class='chat-group-user__name'>${user_name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    group_users_list.append(html);
+}
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -34,5 +43,23 @@ function appendUser(user) {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  $("#user-search-result").on("click", ".user-search-add", function(){
+    var user_id = $(this).attr("data-user-id");
+    var user_name = $(this).attr("data-user-name");
+
+    $.ajax({
+      type: 'POST',
+      url: '/groups',
+      data:{ user_ids: user_id}
+    })
+
+    appendGroupUser(user_id, user_name);
+    $(this).parent().remove();
+  });
+
+  $("#chat-group-users").on("click", ".user-search-remove", function(){
+    $(this).parent().remove();
   });
 });
