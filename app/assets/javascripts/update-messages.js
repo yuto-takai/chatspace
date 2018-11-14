c24e5edb82a66c618d2dcca8cf6c3847143bd7e7
takaikunoff <garakuta.guitar.ist@gmail.com>
2018-11-14 18:38:33 +0900

    Pull request correction

--- app/assets/javascripts/update-messages.js
+++ app/assets/javascripts/update-messages.js
@@ -1,36 +1,41 @@
-setInterval(function() {
-  function buildHTML(message){
-    var image = (message.image) ? `<img src= ${message.image}>` : ``
-    var html = ` <div class= "chat__content-message" data-message-id= "${message.id}">
-                  <p class ="chat__content-message-name">
-                    ${message.user_name}
-                    <span class= "chat__content-message-time">${message.created_at}</span>
-                  </p>
-                  <p class= "chat__content-message-body">
-                    ${message.content}
-                  </p>
-                  <div class= "image">
-                    ${image}
-                  </div>
-                </div>`
+function buildHTML(message){
+  var image = (message.image) ? `<img src= ${message.image}>` : ``
+  var html = ` <div class= "chat__content-message" data-message-id= "${message.id}">
+                <p class ="chat__content-message-name">
+                  ${message.user_name}
+                  <span class= "chat__content-message-time">${message.created_at}</span>
+                </p>
+                <p class= "chat__content-message-body">
+                  ${message.content}
+                </p>
+                <div class= "image">
+                  ${image}
+                </div>
+              </div>`
 
-    return html;
-  }
+  return html;
+}
+var interval = setInterval(function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    var lastMessageId = $('.chat__content-message:last').data('messageId')
 
-  $.ajax({
-    url: location.href.json,
-    dataType: 'json'
-  })
+    $.ajax({
+      url: location.href.json,
+      dataType: 'json',
+      data: { id: lastMessageId }
+    })
 
-  .done(function(messages) {
-    var lastId = $()
-    messages.forEach(function(message) {
-      var html = buildHTML(message);
-      $('.chat__content-message-list').append(html)
-      $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
+    .done(function(messages) {
+
+      messages.forEach(function(message) {
+        var html = buildHTML(message);
+        $('.chat__content-message-list').append(html)
+        $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
+      });
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
     });
-  })
-  .fail(function() {
-    alert('自動更新に失敗しました');
-  });
-}, 1000 * 5);
+  } else {
+  clearInterval(interval);
+}}, 1000 * 5);
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -1,4 +1,4 @@
-json.array! @messages do |message|
+json.array! @new_messages do |message|
   json.id message.id
   json.user_name message.user.name
   json.content message.content
