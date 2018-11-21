80c9cfc671fa457147ca29ee55c44980f93f0c4e
takaikunoff <garakuta.guitar.ist@gmail.com>
2018-11-21 13:02:18 +0900

    delete turbolinks

--- app/assets/javascripts/send-messages.js
+++ app/assets/javascripts/send-messages.js
@@ -1,47 +1,46 @@
-$(document).on('turbolinks:load', function() {
-  $(function(){
-    function buildHTML(message){
-      var image = (message.image) ? `<img src= ${message.image}>` : ``
-      var html = ` <div class= "chat__content-message" data-message-id= "${message.id}">
-                    <p class ="chat__content-message-name">
-                      ${message.user_name}
-                      <span class= "chat__content-message-time">${message.date}</span>
-                    </p>
-                    <p class= "chat__content-message-body">
-                      ${message.content}
-                    </p>
-                    <div class= "image">
-                      ${image}
-                    </div>
-                  </div>`
 
-      return html;
-    }
-    $('#new__message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.chat__content-message-list').append(html)
-        $('.form__message').val('')
-        $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
-      })
-      .fail(function(){
-        alert('メッセージを送信できませんでした')
-      })
-      .always(function(){
-        $(".form__submit").removeAttr("disabled");
-        $('.form__message').val('')
-      });
+$(function(){
+  function buildHTML(message){
+    var image = (message.image) ? `<img src= ${message.image}>` : ``
+    var html = ` <div class= "chat__content-message" data-message-id= "${message.id}">
+                  <p class ="chat__content-message-name">
+                    ${message.user_name}
+                    <span class= "chat__content-message-time">${message.date}</span>
+                  </p>
+                  <p class= "chat__content-message-body">
+                    ${message.content}
+                  </p>
+                  <div class= "image">
+                    ${image}
+                  </div>
+                </div>`
+
+    return html;
+  }
+  $('#new__message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat__content-message-list').append(html)
+      $('.form__message').val('')
+      $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
+    })
+    .fail(function(){
+      alert('メッセージを送信できませんでした')
+    })
+    .always(function(){
+      $(".form__submit").removeAttr("disabled");
+      $('.form__message').val('')
     });
   });
 });
--- app/assets/javascripts/update-messages.js
+++ app/assets/javascripts/update-messages.js
@@ -1,42 +1,41 @@
-$(document).on('turbolinks:load', function() {
-  function buildHTML(message){
-    var image = (message.image) ? `<img src= ${message.image}>` : ``
-    var html = ` <div class= "chat__content-message" data-message-id="${message.id}">
-                  <p class ="chat__content-message-name">
-                    ${message.user_name}
-                    <span class= "chat__content-message-time">${message.date}</span>
-                  </p>
-                  <p class= "chat__content-message-body">
-                    ${message.content}
-                  </p>
-                  <div class= "image">
-                    ${image}
-                  </div>
-                </div>`
 
-    return html;
-  }
-  var interval = setInterval(function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-      var lastMessageId = $('.chat__content-message:last').attr('data-message-id') || 0
-      $.ajax({
-        url: location.href,
-        dataType: 'json',
-        data: { id: lastMessageId }
-      })
+function buildHTML(message){
+  var image = (message.image) ? `<img src= ${message.image}>` : ``
+  var html = ` <div class= "chat__content-message" data-message-id="${message.id}">
+                <p class ="chat__content-message-name">
+                  ${message.user_name}
+                  <span class= "chat__content-message-time">${message.date}</span>
+                </p>
+                <p class= "chat__content-message-body">
+                  ${message.content}
+                </p>
+                <div class= "image">
+                  ${image}
+                </div>
+              </div>`
 
-      .done(function(messages) {
-        messages.forEach(function(message) {
-          console.log(lastMessageId);
-          var html = buildHTML(message);
-          $('.chat__content-message-list').append(html)
-          $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
-        });
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
+  return html;
+}
+var interval = setInterval(function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    var lastMessageId = $('.chat__content-message:last').attr('data-message-id') || 0
+    $.ajax({
+      url: location.href,
+      dataType: 'json',
+      data: { id: lastMessageId }
+    })
+
+    .done(function(messages) {
+      messages.forEach(function(message) {
+        console.log(lastMessageId);
+        var html = buildHTML(message);
+        $('.chat__content-message-list').append(html)
+        $('.chat__content').animate({scrollTop: $('.chat__content')[0].scrollHeight}, 'slow','swing');
       });
-    } else {
-    clearInterval(interval);
-  }}, 1000 * 5);
-});
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  } else {
+  clearInterval(interval);
+}}, 1000 * 5);
