7a8ef964f0fb2d624bb683a60b7676324e467a8c
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-03 18:11:48 +0900

    鍵を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/takaikunoff.pem']
+                  keys: ['~/.ssh/chatspacepem.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
