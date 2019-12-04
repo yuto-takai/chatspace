ba3208eaf63d8d49996a62f5ef2ce3b4a49dfac8
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-04 17:06:20 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 app_path = File.expand_path('../../../', __FILE__)
-worker_processes 1
+worker_processes 2
 working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
