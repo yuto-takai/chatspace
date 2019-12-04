4de3c30674734c0c60e36cae077f27cf4631fd0a
YutoTakai <garakuta.guitar.ist@gmail.com>
2019-12-04 13:40:08 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,6 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-listen 3000
 timeout 60
 
 preload_app true
