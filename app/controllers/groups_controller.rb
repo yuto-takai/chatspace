d76608dd12346a99323e7945c3d127e943170fc9
YutoTakai <garakuta.guitar.ist@gmail.com>
2020-01-15 08:32:44 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,7 +14,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.users << current_user
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path, notice: "グループを作成しました"
     else
       render :new
     end
